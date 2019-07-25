18bbca27dd8b45d822ea20f05cdb15adcd701ce7
Gajadeesh <gaja.ramanathan@gmail.com>
2019-07-25 10:57:02 +0530

    Deploy commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const port = process.env.PORT || 8080;
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + "/dist/"));
+
+app.get(/.*/,function(req, res){
+    res.sendfile(__dirname + "/dist/index.html");
+});
+
+app.listen(port);
+
+console.log("server started");
