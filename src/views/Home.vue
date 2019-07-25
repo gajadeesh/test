426f1b4a0f2040e92220bd78eb79072b936d2db8
Gajadeesh <gaja.ramanathan@gmail.com>
2019-07-25 11:41:18 +0530

    Deploy 3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome the Shop" />
+    <HelloWorld msg="Welcome Balla" />
   </div>
 </template>
 
